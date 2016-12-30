d311c45c3717e4746a50e348eed0e1379f581703
Artur Majewski <artur.majewski2@gmail.com>
2016-12-30 22:04:54 +0100

    Create a base for controller wrapper for SteamVR_TrackedController.

---
+++ Assets/SteamVRWrapper/ControllerWrapper.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerWrapper : SteamVR_TrackedController {
+
+
+    public SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)controllerIndex); } }
+    public Vector3 velocity { get { return controller.velocity; } }
+    public Vector3 angularVelocity { get { return controller.angularVelocity; } }
+
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        // TODO: implement
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+        // TODO: implement
+	}
+
+    public override void OnTriggerClicked(ClickedEventArgs e)
+    {
+        base.OnTriggerClicked(e);
+    }
+
+    public override void OnTriggerUnclicked(ClickedEventArgs e)
+    {
+        base.OnTriggerUnclicked(e);
+    }
+
+    public override void OnMenuClicked(ClickedEventArgs e)
+    {
+        base.OnMenuClicked(e);
+    }
+
+    public override void OnMenuUnclicked(ClickedEventArgs e)
+    {
+        base.OnMenuUnclicked(e);
+    }
+
+    public override void OnSteamClicked(ClickedEventArgs e)
+    {
+        base.OnSteamClicked(e);
+    }
+
+    public override void OnPadClicked(ClickedEventArgs e)
+    {
+        base.OnPadClicked(e);
+    }
+
+    public override void OnPadUnclicked(ClickedEventArgs e)
+    {
+        base.OnPadUnclicked(e);
+    }
+
+    public override void OnPadTouched(ClickedEventArgs e)
+    {
+        base.OnPadTouched(e);
+    }
+
+    public override void OnPadUntouched(ClickedEventArgs e)
+    {
+        base.OnPadUntouched(e);
+    }
+
+    public override void OnGripped(ClickedEventArgs e)
+    {
+        base.OnGripped(e);
+    }
+
+    public override void OnUngripped(ClickedEventArgs e)
+    {
+        base.OnUngripped(e);
+    }
+}
