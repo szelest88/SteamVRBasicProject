c0c9dffcc4295a5771dc9e3567d2af0490d96622
Artur Majewski <artur.majewski2@gmail.com>
2017-03-19 21:15:32 +0100

    Add a listener for info when the play area is determined.

---
+++ Assets/VrapperSizeTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class VrapperSizeTester : MonoBehaviour {
+
+    ChaperoneInfo ci;
+	// Use this for initialization
+	void Start () {
+        ci = ChaperoneInfo.instance; // causes some logs to appear (with accurate data on play area size...)
+        ChaperoneInfo.Initialized.AddListener(new UnityEngine.Events.UnityAction(logInfoAboutSize));
+        
+    }
+
+    // it's delayed
+    void logInfoAboutSize() {
+        Debug.Log("Size X: " + ci.playAreaSizeX);
+        Debug.Log("Size Z: " + ci.playAreaSizeZ);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
